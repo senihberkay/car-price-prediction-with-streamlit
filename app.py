9f2564787df3f205c6674a9179349ba40b8182c5
Senih Berkay Akın <99797076+senihberkay@users.noreply.github.com>
2022-09-24 03:08:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,61 @@
-
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
+
 import pickle
-from sklearn.linear_model import Lasso
-from sklearn.compose import make_column_transformer
-from sklearn.preprocessing import OneHotEncoder, MinMaxScaler
-from sklearn.pipeline import Pipeline
 
-st.markdown("<h2 style='text-align:center; color:Black;'>Car Price Prediction</h2>", unsafe_allow_html=True)
-
-df = pd.read_csv("final_scout.csv")
-
-features = ["make_model", "hp_kW", "km","age", "Gearing_Type", "Gears","price"]
-df = df[features]
-
-if st.checkbox("Show Dataframe"):
-    st.write(df.head())
-
-X = df.drop(columns = ["price"])
-y = df.price
-
-cat = X.select_dtypes("object").columns
-cat = list(cat)
-
-column_trans = make_column_transformer((OneHotEncoder(handle_unknown="ignore", sparse=False), cat), 
-                                       remainder=MinMaxScaler())
-
-operations = [("OneHotEncoder", column_trans), ("Lasso", Lasso(alpha = 0.01))]
-pipe_model = Pipeline(steps=operations)
-pipe_model.fit(X, y)
-
-pickle.dump(pipe_model, open('autoscout_project.pkl', 'wb'))
- 
-st.sidebar.title("Please select the features you want for price estimation")
-
-features = ["make_model", "hp_kW", "km","age", "Gearing_Type", "Gears","price"]
-
-def user_input_features() :
-    make_model = st.sidebar.selectbox("Make Model", ("Audi A3","Audi A1","Opel Insignia", "Opel Astra", "Opel Corsa", "Renault Clio", "Renault Espace", "Renault Duster"))
-    Gearing_Type = st.sidebar.selectbox("Gearing Type", ("Manual","Automatic", "Semi-automatic"))
-    age = st.sidebar.number_input("Age:",min_value=0, max_value=3)
-    Gears = st.sidebar.radio("Gears",(5,6,7,8))
-    hp_kW = st.sidebar.slider("Horse Power(kW)", df["hp_kW"].min(), df["hp_kW"].max(), float(df["hp_kW"].median()),1.0)
-    km = st.sidebar.slider("Kilometer(km)", df["km"].min(), df["km"].max(), float(df["km"].median()),1.0)
-    data = {"make_model" : make_model,
-            "Gearing_Type" : Gearing_Type,
-            "age" : age,
-            "hp_kW" : hp_kW,
-            "km" : km,
-            "Gears" : Gears}
-    features = pd.DataFrame(data, index=[0])
-    return features
+html_temp = """
+<div style="background-color:yellow;padding:1.5px">
+<h1 style="color:black;text-align:center;">Used Car Price Prediction</h1>
+</div><br>"""
+st.markdown(html_temp, unsafe_allow_html=True)
 
 
 
-input_df = user_input_features()
-st.success("Selected Features")
-st.write(input_df)
+img = Image.open("AutoScout_Logo.png")
+st.image(img, caption="")
 
-model = pickle.load(open("final_model_scout.pkl", "rb"))
+st.write("\n\n"*2)
 
-#Apply model to make predictions
-if st.button('Make Prediction'):
-    st.success(f'Analyze Predict:&emsp;{model.predict(input_df)[0].round(2)}')
+filename = 'Auto_Price_Pred_Model'
+model = pickle.load(open(filename, 'rb'))
+
+with st.sidebar:
+    st.subheader('Car Specs to Predict Price')
+
+make_model = st.sidebar.selectbox("Model Selection", ("Audi A3", "Audi A1", "Opel Insignia", "Opel Astra", "Opel Corsa", "Renault Clio", "Renault Espace", "Renault Duster"))
+hp_kW = st.sidebar.number_input("Horse Power:",min_value=40, max_value=294, value=120, step=5)
+age = st.sidebar.number_input("Age:",min_value=0, max_value=3, value=0, step=1)
+km = st.sidebar.number_input("km:",min_value=0, max_value=317000, value=10000, step=5000)
+Gears = st.sidebar.number_input("Gears:",min_value=5, max_value=8, value=5, step=1)
+Gearing_Type = st.sidebar.radio("Gearing Type", ("Manual", "Automatic", "Semi-automatic"))
+
+
+my_dict = {"make_model":make_model, "hp_kW":hp_kW, "age":age, "km":km, "Gears":Gears, "Gearing_Type":Gearing_Type}
+df = pd.DataFrame.from_dict([my_dict])
+
+cols = {
+    "make_model": "Car Model",
+    "hp_kW": "Horse Power",
+    "age": "Age",
+    "km": "km Traveled",
+    "Gears": "Gears",
+    "Gearing_Type": "Gearing Type"
+}
+
+df_show = df.copy()
+df_show.rename(columns = cols, inplace = True)
+st.write("Selected Specs: \n")
+st.table(df_show)
+
+
+if st.button("Predict"):
+    pred = model.predict(df)
+    col1, col2 = st.columns(2)
+    col1.write("The estimated value of car price is €")
+    col2.write(pred[0].astype(int))
+
+
+st.write("\n\n")
+
